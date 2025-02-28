80adb600564bc17354c5d49a2892a507e9a392f4
Swati07M <mahajanswati1996@gmail.com>
2025-02-28 20:29:15 +0530

    portfolio

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+document.getElementById("download-btn").addEventListener("click", function(event) 
+  {
+  window.open("./assets/SwatiMahajan-WebEngineer-Emp_681.pdf", "_blank");
+  const anchor = document.createElement("a");
+  anchor.href = "./assets/SwatiMahajan-WebEngineer-Emp_681.pdf";
+  anchor.download ="SwatiMahajan-Resume.pdf";
+  document.body.appendChild(anchor);
+  anchor.click();
+  document.body.removeChild(anchor);
+  });
+  document.getElementById("download-cv-btn").addEventListener("click", function() {
+    const fileUrl = "./assets/SwatiMahajan-WebEngineer-Emp_681.pdf";
+    window.open(fileUrl, "_blank");
+    const anchor = document.createElement("a");
+    anchor.href = fileUrl;
+    anchor.download = "SwatiMahajan-CV.pdf"; 
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+  });
